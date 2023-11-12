d8f3ac11cfed7d8dae420ee5331075ebb315ded3
mattsheng <mattsheng04165@gmail.com>
2023-11-12 09:29:31 -0600

    Updated vignettes to pull cached result instead of running full example during packing build.

--- R/data.R
+++ R/data.R
@@ -1,7 +1,6 @@
 #' Single-Atom Catalysis Data
 #'
-#' @format
-#' A list with 4 objects:
+#' @format A list with 4 objects:
 #' \describe{
 #'   \item{X}{Primary feature matrix: physical properties of transition metals and oxide supports}
 #'   \item{y}{Reponse variable: binding energy of metal/oxide pairs}
@@ -9,3 +8,27 @@
 #'   \item{unit}{Unit of columns of X}
 #' }
 "catalysis"
+
+#' iBART Real Data Result
+#'
+#' @description
+#' iBART result in the real data vignette
+#'
+#' @format A list of iBART outputs
+#' \describe{
+#'   \item{iBART_model}{A cv.glmnet object storing the iBART selected model}
+#'   ...
+#' }
+"iBART_real_data"
+
+#' iBART Simulation Result
+#'
+#' @description
+#' iBART result in the simulation vignette
+#'
+#' @format A list of iBART outputs
+#' \describe{
+#'   \item{iBART_model}{A cv.glmnet object storing the iBART selected model}
+#'   ...
+#' }
+"iBART_sim"
